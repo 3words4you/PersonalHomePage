080ec38a36addb4ebb5485aafa6a82ac2a7a1622
Yi Mo <1197967396@qq.com>
2018-07-24 15:29:31 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app-main">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+const COM_NAME = 'app';
+export default {
+  name: COM_NAME,
+  mounted() {
+  },
+  beforeDestroy() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="stylus">
+  html,body
+    width: 100%
+    height: 100%
+    background: #f0f0f0
+    overflow: hidden
+    margin: 0
+    padding: 0
+
+  .app-main
+    width: 100%
+    height: 100%
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <div class="header">
+      <div class="my-logo">
+        Yi's Personal Home Page
+      </div>
+      <div class="navTabGroup">
+        <div class="navTabItem">
+          Home
+        </div>
+        <div class="navTabItem">
+          Resume
+        </div>
+        <div class="navTabItem">
+          Projects Demo
+        </div>
+      </div>
+    </div>
+    <div class="content">
+
+    </div>
+    <div class="footer">
+
+    </div>
+	</div>
+</template>
+
+<script>
+  /* eslint-disable */
+	export default {
+		name: 'home'
+	};
+</script>
+
+<style lang="stylus">
+  .home
+    width: 100%
+    height: 100%
+    background-image: url('https://file.iviewui.com/iview-admin/login_bg.jpg')
+    background-size: cover
+    background-position: center
+    position: absolute
+  .header
+    display: flex
+    height: 80px
+    justify-content: space-between
+    padding: 0 30px
+    font-size: 20px;
+    line-height: 80px
+    .my-logo
+      color: white
+    .navTabGroup
+      color: white
+      display: flex
+      margin-right: 20px
+    .navTabItem
+      margin-right: 40px
+
+</style>
