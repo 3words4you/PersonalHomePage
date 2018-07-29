5ade62b1e5c4904be482b3d8b5b850326a474fca
Yi Mo <1197967396@qq.com>
2018-07-28 20:39:31 -0400

    update project page, build

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -8,16 +8,21 @@
         <p v-for="(item,index) in categoryList" class="category_item" :class="{'active' : item == activeCategory}" @click="categoryClick(item)">{{item}}</p>
       </div>
       <div class="projects_page_projects_box">
-        <div v-for="item in filteredProjects" class="project_item">
-          <img :src="item.image" />
+        <div v-for="item in filteredProjects" class="project_item hovereffect">
+          <img  :src="item.image" alt="image load failed">
+          <div class="overlay">
+             <h2>{{item.name}}</h2>
+             <p>{{item.desc}}</p>
+             <p @click="openNewWindow(item.link)" class="link">View More</p>
+          </div>
         </div>
       </div>
-      <div class="fixed_navTags_box" @click="goToHome">
-        <i class="fas fa-home fa-2x"></i>
-        <p>
-          Home
-        </p>
-      </div>
+    </div>
+    <div class="fixed_navTags_box" @click="goToHome">
+      <i class="fas fa-home fa-2x"></i>
+      <p>
+        Home
+      </p>
     </div>
   </div>
 </template>
@@ -25,19 +30,26 @@
 <script>
   /* eslint-disable */
   import projectCommunityImg from '../assets/images/projectCommunity.jpg';
+  import projectZhuazhuakanImg from '../assets/images/projectZhuazhuakan.jpg';
+  import projectCMSImg from '../assets/images/projectCMS.jpg';
+  import projectYutianxiaImg from '../assets/images/projectYutianxia.jpg';
 	export default {
 		name: 'projects',
     data() {
       return {
         projectCommunityImg,
+        projectZhuazhuakanImg,
+        projectCMSImg,
+        projectYutianxiaImg,
         activeCategory: 'All',
         categoryList: [
           'All', 'Website', 'CMS', 'Mobile App', 'Game'
         ],
         projectList: [
-          {name: 'Community Web', category: 'Website', image: projectCommunityImg, link: 'https://community.zoombin.com/webpc'},
-          {name: 'Catch Web', category: 'Mobile App', image: projectCommunityImg, link: 'https://community.zoombin.com/webpc'},
-          {name: 'Catch Web', category: 'Mobile App', image: projectCommunityImg, link: 'https://community.zoombin.com/webpc'},
+          {name: 'E-cigs Community', category: 'Website', image: projectCommunityImg, link: 'https://community.zoombin.com/webpc', desc: 'E-cigs Community is a Social Question & Answer Website, in which e-cigarettes users can share, ask or answer topics about e-cigs. Reasons for using e-cigarettes involve trying to quit smoking, reduce risk, or save money, though some use them recreationally.'},
+          {name: 'Yu Tian Xia', category: 'Mobile App', image: projectYutianxiaImg, link: 'https://yutianxia.zoombin.com', desc: 'Yu Tian Xia is a jade E-commerce platform. Currently, there are 1000+ shops, 1000+ masters and 4500+ customers registered in platform'},
+          {name: 'Claw Crane OL', category: 'Mobile App, Website', image: projectZhuazhuakanImg, link: 'https://zhuazhuakan.zoombin.com', desc: 'Claw crawn is an online game. Users can control the claw remotely to catch toys. After a successfully catch, users can request for a delivery or exchange into game coins. First time login will get 3 free chances immediately! Game is only available by using WeChat App or QQ broswer'},
+          {name: 'Content Management System', category: 'Website, CMS', image: projectCMSImg, link: 'https://community.zoombin.com/admin', desc: 'Developed CMSs to managing contents for applications'},
         ]
       }
     },
@@ -46,7 +58,7 @@
         if(this.activeCategory == 'All'){
           return this.projectList;
         }
-        return this.projectList.filter(project => project.category == this.activeCategory);
+        return this.projectList.filter(project => project.category.includes(this.activeCategory));
       }
     },
     methods: {
@@ -57,12 +69,16 @@
         this.$router.push({
           path: '/'
         })
+      },
+      openNewWindow(url) {
+        window.open(url);
       }
     }
 	};
 </script>
 
 <style lang="stylus">
+  @import "../assets/css/hoverEffects.styl"
   ::-moz-selection /* Code for Firefox */
     color: white
     background: #4bc37b
@@ -108,7 +124,7 @@
         color: white
         background: #4bc37b
     &_projects_box
-      width: 800px
+      width: 960px
       margin: 30px auto 0 auto
       display: flex
       flex-wrap: wrap
@@ -138,11 +154,11 @@
         color: #3e424a
         font-weight: 100
         font-size: 10px
-  @media only screen and (max-width: 800px)
+  @media only screen and (max-width: 960px)
     .projects_page
       box-sizing: border-box
-      &_projects_box
-        width: 100%
+      &_content_box
+        width: 88%
       &_categories_box
         margin: 10px 0 0 0
         color: #3e424a
@@ -152,7 +168,7 @@
         align-items: center
         text-align: center
         white-space: nowrap
-        width: 88%
+        width: 100%
         .category_item
           width: 100%
           margin: 0
@@ -161,6 +177,11 @@
           border-radius: 4px
           color: white
           background: #4bc37b
-
+      &_projects_box
+        width: 100%
+        .project_item
+          margin: 0 0 20px 0
+          padding: 0
+          width: 88vw
+          height: auto
 </style>
-19201920
